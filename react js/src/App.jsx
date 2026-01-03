b7b6ee5fa72f0762f9767ebd6111c9b20989b9e3
Miruthula112006 <miruthulav11@gmail.com>
2026-01-03 10:50:34 +0530

    to do list add and delete

--- react js/src/App.jsx
+++ react js/src/App.jsx
@@ -4,9 +4,71 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tasks, setTasks] = useState([]);
+  const [text,setText] = useState(" ");
 
-  return (
+  const addTask=(e)=>{
+    e.preventDefault();
+    const trimmed =text.trim();
+    if(!trimmed)return;
+
+    const newTask = {
+      id: Date.now(),
+      text: trimmed,
+      completed: false
+    };
+
+    setTasks([...tasks, newTask]);
+    setText(" ");
+  };
+
+  const deleteTask=(id)=>{
+    setTasks(tasks.filter((t)=>t.id!==id));
+  };
+
+  const toggleTask = (id)=>{
+    setTasks(
+      tasks.map((t)=>
+        t.id===id?{...t,completed: !t.completed}:t
+    )
+  );
+  };
+
+  return(
+    <div className='app'>
+      <h1>To-Do-List</h1>
+      <form onSubmit={addTask} className='task-form'>
+        <input 
+        value={text}
+        onChange={(e)=>setText(e.target.value)}
+        placeholder='enter new task' 
+        aria-label='Task'
+        />
+        <button type='submit'>Add</button>
+      </form>
+      <ul className='task-list'>
+        {tasks.length===0 && <li className='empty'>No tasks yet</li>}
+        {tasks.map((task)=>{
+          <li key={task.id} className='task-name'>
+            <input
+            type="checkbox" checked={task.completed}
+            onChange={()=>toggleTask(task.id)}
+            />
+            <span onClick={()=>toggleTask(task.id)}
+            className={task.completed? "done":""}>
+              {task.text}
+            </span>
+            <button className='delete' onClick={()=>deleteTask(task.id)}>Delete</button>
+          </li>
+        })}
+
+      </ul>
+
+    </div>
+  );
+}
+
+ /* return (
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
@@ -30,6 +92,6 @@ function App() {
       </p>
     </>
   )
-}
+} */
 
 export default App
